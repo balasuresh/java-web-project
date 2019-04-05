33c3bc899ace0a10e7b62880d84afb45e750c8eb
Jyothi <lalithajyothi.160@gmail.com>
2019-04-06 00:05:40 +0530

    commit

--- src/test/java/com/mkyong/web/TestWelcome.java
+++ src/test/java/com/mkyong/web/TestWelcome.java
@@ -44,10 +44,12 @@ public class TestWelcome {
                 .andExpect(model().attribute("msg", "Hello World"));
     }
 
+    // this is dummy test added ********************************** 
     @Test public void helloWorld() throws Exception {
         mockMvc.perform(get("/hello").accept(MediaType.TEXT_PLAIN))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_PLAIN))
                 .andExpect(content().string("Hello World!"));
     }
+    // this is dummy test added ********************************** 
 }
