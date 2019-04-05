cedd18b57364e8afa36f201f71873988531e6480
Jyothi <lalithajyothi.160@gmail.com>
2019-04-05 18:57:07 +0530

    commit

--- src/test/java/com/mkyong/web/TestWelcome.java
+++ src/test/java/com/mkyong/web/TestWelcome.java
@@ -2,12 +2,16 @@ package com.mkyong.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -40,4 +44,10 @@ public class TestWelcome {
                 .andExpect(model().attribute("msg", "Hello World"));
     }
 
+    @Test public void helloWorld() throws Exception {
+        mockMvc.perform(get("/hello").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("Hello World!"));
+    }
 }
